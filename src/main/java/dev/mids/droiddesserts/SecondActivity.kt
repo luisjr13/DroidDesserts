0e14c2880da2b30e493b7de0ba3901577d35f8b5
Luis Soares <junior130977@hotmail.com>
2023-06-04 08:27:58 -0300

    Correção de erros

--- src/main/java/dev/mids/droiddesserts/SecondActivity.kt
+++ src/main/java/dev/mids/droiddesserts/SecondActivity.kt
@@ -11,14 +11,13 @@ class SecondActivity: AppCompatActivity () {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-       var ImageId = intent.getIntExtra(MainActivity.KEY_IMAGE, R.drawable.ic_launcher_background)
-        val ImageId= intent.getIntExtra(MainActivity.KEY_IMAGE_NAME).orEmpty()
-
+       val imageId = intent.getIntExtra(MainActivity.KEY_IMAGE, R.drawable.ic_launcher_background)
+        val imageName= intent.getStringExtra(MainActivity.KEY_IMAGE_NAME).orEmpty()
         val ivimage = findViewById<ImageView>(R.id.ivDroidVersion)
         val tvImageName: TextView = findViewById(R.id.tvNameImagem)
 
-        ivimage.setImageResource(ImageId)
-        tvImageName.text = tvImageName
+        ivimage.setImageResource(imageId)
+        tvImageName.text= imageName
     }
 
 }
